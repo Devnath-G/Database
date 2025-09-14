316b3d6dc87a0650d533b2c3cbb771f263ce2172
Devnath G <152295574+Devnath-G@users.noreply.github.com>
2025-09-14 16:23:37 +0530

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -1,11 +1,4 @@
-export DEVICE=/dev/dri/renderD128
-export DEVICE_GRP=$(ls -g $DEVICE | awk '{print $3}' | xargs getent group | awk -F: '{print $3}')
-
-docker run -d \
-   --name facility-v1 \
-   --restart=always \
-   --net=host \
-   --device /dev/dri --group-add ${DEVICE_GRP} \
+sudo docker run -d --name facility-v1 --restart=always --net=host \
    -v /etc/timezone:/etc/timezone:ro \
    -v /etc/localtime:/etc/localtime:ro \
    -v /home/difinative/Management:/home/metro/Management \
